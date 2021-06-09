8141c9319fa96f3ac4097ad220bb9b583fc98439
David Pham <35905105+basespl1tyt@users.noreply.github.com>
2021-06-09 17:56:15 -0400

    New Website Upload

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,32 +1,33 @@
     document.write( `
-    <header id="header" class="fixed-top d-flex align-items-center">
-    <div class="container d-flex align-items-center">
+    <header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-      <div class="logo me-auto">
-      <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
-      </div>
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/logo.png" alt="">
+      </a>
 
-      <nav class="nav-menu d-none d-lg-block">
+      <nav id="navbar" class="navbar">
         <ul>
-          <li class="active"><a href="index.html">Home</a></li>
-          <li><a href="#about">About</a></li>
-          
-          <li class="drop-down"><a href="#portfolio">Airports</a>
+          <li><a class="nav-link scrollto active" href="index.html#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="index.html#about">About</a></li>
+          <li><a class="nav-link scrollto" href="index.html#features">Features</a></li>
+          <li class="dropdown"><a href="#pricing"><span>Airports</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="phli-details.html">PHLI - Lihue Airport</a></li>
-              <li><a href="cykf-details.html">CYKF - Waterloo</a></li>
-              <li><a href="cnc4-details.html">CNC4 - Guelph Airpark</a></li>
-              <li><a href="czba-details.html">CZBA - Burlington Airpark</a></li>
-              <li><a href="phog-details.html">PHOG - Kahului Airport</a></li>
-
+              <li><a href="phli.html">PHLI - Lihue Airport</a></li>
+              <li><a href="cykf.html">CYKF - Waterloo Airport</a></li>
+              <li><a href="cnc4.html">CNC4 - Guelph Airpark</a></li>
+              <li><a href="kpvu.html">KPVU - Provo Muni</a></li>
             </ul>
           </li>
+          <li><a class="nav-link scrollto" href="index.html#testimonials">Testinomials</a></li>
+          <li><a class="nav-link scrollto" href="index.html#team">Team</a></li>
+          <li><a class="nav-link scrollto" href="index.html#recent-blog-posts">News</a></li>
 
-          <li><a href="#contact">Contact</a></li>
-          <li class="get-started"><a href="https://secure.simmarket.com/regdesigns.mhtml">Buy Now</a></li>
+          <li><a class="getstarted scrollto" href="https://secure.simmarket.com/regdesigns.mhtml">Buy Now</a></li>
         </ul>
-      </nav><!-- .nav-menu -->
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
 
     </div>
-  </header><!-- End Header -->
+  </header>
     `);
